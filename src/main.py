0ffaf54f33dc9757cf1ed949225a694209a13501
Arthur Duarte Oliveira <arthur.duarte.oli06@gmail.com>
2025-10-24 13:58:30 -0300

    Added stratified Cross Validation and baseline scoring

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from data_processing import load_data, initial_preprocess
 # from model_training import 
-# from evaluation import 
+from evaluation import get_cv_scores
 import sys
 from model_training import split_data
+from sklearn.linear_model import LogisticRegression
 
 file = 'data/creditcard.csv'
 
@@ -13,7 +14,8 @@ if df is not None:
     df_new = initial_preprocess(df)
     if df_new is not None:
         X_train, X_test, y_train, y_test = split_data(df_new)
-        print(X_train.shape)
+        baseline_model = LogisticRegression()
+        print(get_cv_scores(baseline_model, X_train, y_train))
     else:
         print('An error occurred during the preprocessing')
         sys.exit(1)
