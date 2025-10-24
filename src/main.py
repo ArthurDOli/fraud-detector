74e1d034b95fd0c2d61f0f8f1876117341eb7395
Arthur Duarte Oliveira <arthur.duarte.oli06@gmail.com>
2025-10-24 15:12:27 -0300

    Created smote pipeline and analyzed the final metrics

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from data_processing import load_data, initial_preprocess
 # from model_training import 
 from evaluation import get_cv_scores
 import sys
-from model_training import split_data
+from model_training import split_data, create_smote_rf_pipeline
 from sklearn.linear_model import LogisticRegression
 
 file = 'data/creditcard.csv'
@@ -15,11 +15,11 @@ if df is not None:
     if df_new is not None:
         X_train, X_test, y_train, y_test = split_data(df_new)
         baseline_model = LogisticRegression()
-        print(get_cv_scores(baseline_model, X_train, y_train))
+        smote_model = create_smote_rf_pipeline()
+        print(get_cv_scores(smote_model, X_train, y_train))
     else:
         print('An error occurred during the preprocessing')
         sys.exit(1)
 else:
     print('Fail loading the data')
     sys.exit(1)
-
--- src/model_training.py
+++ src/model_training.py
@@ -1,10 +1,39 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from typing import Tuple
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 # Data division
 def split_data(df_processed: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Splits the preprocessed DataFrame into training and testing sets
+
+    df_processed (pd.DataFrame): The clean DataFrame (after initial_preprocess)
+    """
     y: pd.Series = df_processed['Class']
     X: pd.DataFrame = df_processed.drop('Class', axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=42)
     return X_train, X_test, y_train, y_test
+
+def create_smote_pipeline() -> Pipeline:
+    """
+    Creates a imbalanced-learn (imblearn) pipeline that aplies SMOTE and trains an LogisticRegression classifier
+    """
+    pipeline: Pipeline = Pipeline(steps=[
+        ('smote', SMOTE(random_state=42)),
+        ('model', LogisticRegression(random_state=42, max_iter=1000))
+    ])
+    return pipeline
+
+def create_smote_rf_pipeline():
+    """
+    Creates a imbalanced-learn (imblearn) pipeline that aplies SMOTE and trains an RandomForest classifier
+    """
+    pipeline = Pipeline(steps=[
+        ('smote', SMOTE(random_state=42)),
+        ('random_forest', RandomForestClassifier(random_state=42, n_jobs=-1))
+    ])
+    return pipeline
